a74cad4fbfff788dfc60d93698e299b28000957f
Adriano Gil <adrianogil.san@gmail.com>
2020-12-13 17:22:35 -0400

    Autoformat install script using black

--- src/python/gil_install.py
+++ src/python/gil_install.py
@@ -1,10 +1,22 @@
-#!/usr/bin/env python2
-import sys, json, os, subprocess
+#!/usr/bin/env python3
+"""
+    Script that automatically install Gil projects by:
+    - creating a link to project shellscript from main .bashrc.sh
+
+"""
+from subprocess import *
+
+import subprocess
+import json
+import sys
+import os
+
 
 try:
     range = xrange
 except NameError:
     pass
+
 
 def is_float(s):
     try:
@@ -15,16 +27,16 @@ def is_float(s):
 
     return False
 
-from subprocess import *
 
 def get_git_root(p):
     """Return None if p is not in a git repo, or the root of the repo if it is"""
-    if call(["git", "branch"], stderr=STDOUT, stdout=open(os.devnull, 'w'), cwd=p) != 0:
+    if call(["git", "branch"], stderr=STDOUT, stdout=open(os.devnull, "w"), cwd=p) != 0:
         return None
     else:
         root = check_output(["git", "rev-parse", "--show-toplevel"], cwd=p)
         root = root.strip()
         return root
+
 
 def get_git_url():
     git_url_cmd = "git config --get remote.origin.url"
@@ -32,6 +44,7 @@ def get_git_url():
     git_url_output = git_url_output.strip()
 
     return git_url_output
+
 
 class GilInstallController:
     def __init__(self):
@@ -46,17 +59,19 @@ class GilInstallController:
         install_info = {}
 
         if len(args) < 1:
-            print("Error: you should provide at least the name of MACRO that will represent project path")
+            print(
+                "Error: you should provide at least the name of MACRO that will represent project path"
+            )
             return
 
         install_macro = args[0]
 
         print("Using MACRO installation: " + install_macro)
-        install_info['DIR_MACRO'] = install_macro
+        install_info["DIR_MACRO"] = install_macro
 
         if len(args) > 1:
             print("Using BASHRC script: " + args[1])
-            install_info['BASHRC'] = args[1]
+            install_info["BASHRC"] = args[1]
         else:
             find_bashrc_cmd = "find . -name 'bashrc.sh' | head -1"
             find_bashrc_output = subprocess.check_output(find_bashrc_cmd, shell=True)
@@ -65,14 +80,14 @@ class GilInstallController:
             if find_bashrc_output != "":
                 print("Found BASHRC script: " + find_bashrc_output)
 
-                if find_bashrc_output[0:2] == './':
+                if find_bashrc_output[0:2] == "./":
                     find_bashrc_output = find_bashrc_output[2:]
 
-                install_info['BASHRC'] = find_bashrc_output
+                install_info["BASHRC"] = find_bashrc_output
             else:
                 print("No BASHRC script was found!")
 
-        with open(self.installation_file, 'w') as f:
+        with open(self.installation_file, "w") as f:
             json.dump(install_info, f)
 
     def verify_installation(self, args, extra_args):
@@ -81,24 +96,27 @@ class GilInstallController:
         if len(args) > 0:
             target_bashrc = args[0]
 
-        with open(target_bashrc, 'r') as f:
+        with open(target_bashrc, "r") as f:
             install_info = json.load(f)
 
-        if 'DIR_MACRO' not in install_info:
+        if "DIR_MACRO" not in install_info:
             print("Wrong usage!")
             return
 
-        install_macro = install_info['DIR_MACRO']
+        install_macro = install_info["DIR_MACRO"]
 
         found = False
         try:
-            verify_dir_install_cmd = "cat '" + self.config_file + "' | grep '" + \
-                install_macro + "'"
-            verify_dir_install_output = subprocess.check_output(verify_dir_install_cmd, shell=True)
+            verify_dir_install_cmd = (
+                "cat '" + self.config_file + "' | grep '" + install_macro + "'"
+            )
+            verify_dir_install_output = subprocess.check_output(
+                verify_dir_install_cmd, shell=True
+            )
             verify_dir_install_output = verify_dir_install_output.strip()
 
             found = verify_dir_install_output != ""
-        except:
+        except Exception as _:
             # Maybe the export does not exist. So let's create it
             # print("It seems that export config file does not exist. Let's create it")
             found = False
@@ -121,54 +139,59 @@ class GilInstallController:
             else:
                 current_dir = os.path.join(current_dir, find_gilfile_output)
 
-
-        with open(os.path.join(current_dir, self.installation_file), 'r') as f:
+        with open(os.path.join(current_dir, self.installation_file), "r") as f:
             install_info = json.load(f)
 
-        if 'DIR_MACRO' in install_info:
+        if "DIR_MACRO" in install_info:
 
             git_root_path = get_git_root(current_dir)
             git_url = get_git_url()
 
             repo_name = os.path.basename(git_root_path)
 
-            install_macro = install_info['DIR_MACRO']
+            install_macro = install_info["DIR_MACRO"]
 
             found = False
 
             try:
-                verify_dir_install_cmd = "cat '" + self.config_file + "' | grep '" + \
-                        install_macro + "'"
-                verify_dir_install_output = subprocess.check_output(verify_dir_install_cmd, shell=True)
+                verify_dir_install_cmd = (
+                    "cat '" + self.config_file + "' | grep '" + install_macro + "'"
+                )
+                verify_dir_install_output = subprocess.check_output(
+                    verify_dir_install_cmd, shell=True
+                )
                 verify_dir_install_output = verify_dir_install_output.strip()
 
                 found = verify_dir_install_output != ""
-            except:
+            except Exception as _:
                 # Maybe the export does not exist. So let's create it
-                print("It seems that export config file does not exist. Let's create it")
+                print(
+                    "It seems that export config file does not exist. Let's create it"
+                )
                 found = False
 
             if found:
                 print("Project already installed!")
             else:
                 if os.path.exists(self.config_file):
-                    f = open(self.config_file, 'a+')
+                    f = open(self.config_file, "a+")
                 else:
-                    f = open(self.config_file, 'w+')
+                    f = open(self.config_file, "w+")
                 f.write("##### %s #####\n" % (repo_name,))
                 f.write("# GitRepo: %s\n" % (git_url,))
                 f.write("# InstallDir: %s\n" % (git_root_path,))
                 f.write("export %s=%s\n" % (install_macro, current_dir))
 
-                if 'BASHRC' in install_info:
-                    f.write("source $%s/%s\n" % (install_macro, install_info['BASHRC']))
+                if "BASHRC" in install_info:
+                    f.write("source $%s/%s\n" % (install_macro, install_info["BASHRC"]))
                     f.write("\n")
                 f.close()
 
             print("Verify if there aren't any python requirements files")
-            find_requeriments_cmd = "find . -name 'requirements.txt' | xargs -I {} pip install -r {}"
+            find_requeriments_cmd = (
+                "find . -name 'requirements.txt' | xargs -I {} pip install -r {}"
+            )
             subprocess.check_output(find_requeriments_cmd, shell=True)
-
 
     def show_help(self, args, extra_args):
         help_text = "gil-install: generation and installation of projects based on bashrc.sh\n\n"
@@ -179,28 +202,30 @@ class GilInstallController:
 
     def get_commands(self):
         commands_parse = {
-            '-c'           : self.create,
-            '-i'           : self.install,
-            '-h'           : self.show_help,
-            '-v'           : self.verify_installation,
-            '--help'       : self.show_help,
-            '--create'     : self.create,
-            '--install'    : self.install,
-            '--verify'     : self.verify_installation,
+            "-c": self.create,
+            "-i": self.install,
+            "-h": self.show_help,
+            "-v": self.verify_installation,
+            "--help": self.show_help,
+            "--create": self.create,
+            "--install": self.install,
+            "--verify": self.verify_installation,
             # '--list-args'  : self.list_args,
             # '--auto-list'  : self.auto_list,
             # 'no-args'      : self.handle_no_args,
         }
         return commands_parse
 
+
 controller = GilInstallController()
 commands_parse = controller.get_commands()
+
 
 def parse_arguments():
 
     args = {}
 
-    last_key = ''
+    last_key = ""
 
     if len(sys.argv) == 1:
         controller.handle_no_args()
@@ -208,15 +233,16 @@ def parse_arguments():
 
     for i in range(1, len(sys.argv)):
         a = sys.argv[i]
-        if a[0] == '-' and not is_float(a):
+        if a[0] == "-" and not is_float(a):
             last_key = a
             args[a] = []
-        elif last_key != '':
+        elif last_key != "":
             arg_values = args[last_key]
             arg_values.append(a)
             args[last_key] = arg_values
 
     return args
+
 
 def parse_commands(args):
     if args is None:
@@ -226,6 +252,7 @@ def parse_commands(args):
     for a in args:
         if a in commands_parse:
             commands_parse[a](args[a], args)
+
 
 args = parse_arguments()
 parse_commands(args)
